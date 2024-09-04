b914eb4ef8feac3cb56be00d03a07c10b22adbdf
Igor Kovryzhkin <igorkovr@gmail.com>
2024-09-04 19:02:43 +0200

    remove leftover import

--- web/src/app/workflows/page.tsx
+++ web/src/app/workflows/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import FunctionalWrapper from "../chat/shared_chat_search/FunctionalWrapper";
 import { WorkflowsSection } from "./WorkflowsSection";
-import WrappedWorkflows from "./WrappedWorkflows";
 
 export default async function Home() {
   return (
