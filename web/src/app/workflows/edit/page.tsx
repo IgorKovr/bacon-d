d1200bf0a48e1d5b39fd259a374663b6b94cba19
Igor Kovryzhkin <igorkovr@gmail.com>
2024-10-14 15:42:50 +0200

    Remove Sidebar from Workflows

--- web/src/app/workflows/edit/page.tsx
+++ web/src/app/workflows/edit/page.tsx
@@ -17,19 +17,8 @@ export default function EditWorkflowPage() {
       content={() => (
         <>
           <div className="flex relative pr-[8px] h-full text-default">
-            <div className="flex-none fixed left-0 z-30 bg-background-100 h-screen transition-all bg-opacity-80 duration-300 ease-in-out">
-              <div className="w-full relative">
-                <HistorySidebar explicitlyUntoggle={() => {}} page="chat" />
-              </div>
-            </div>
-
             <div className="absolute include-scrollbar h-screen overflow-y-auto left-0 w-full top-[80px] bottom-0">
               <div className="w-full flex">
-                <div
-                  style={{ transition: "width 0.30s ease-out" }}
-                  className="flex-none overflow-y-hidden bg-background-100 h-full transition-all bg-opacity-80 duration-300 ease-in-out w-[250px]"
-                />
-
                 <div className="desktop:px-24 w-full mt-10 pt-10 relative max-w-[2000px] xl:max-w-[1430px] mx-auto">
                   <div className="mobile:fixed mobile:left-1/2 mobile:transform mobile:-translate-x-1/2 mobile:max-w-search-bar-max mobile:w-[90%] mobile:z-100 mobile:bottom-12">
                     <div className="mt-0 mb-8 flex justify-center items-center">
